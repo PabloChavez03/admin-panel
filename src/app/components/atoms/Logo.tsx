e38e9989a01ecc4823faf47c861c60257e8ad502
PabloChavez03 <pablooscarchavez@gmail.com>
2023-08-31 21:51:57 -0300

    add extensible of out

--- src/app/components/atoms/Logo.tsx
+++ src/app/components/atoms/Logo.tsx
@@ -1,6 +1,11 @@
 import eluxLogo from '@/public/elux.svg'
 import Image from 'next/image'
+import React from 'react'
 
-export const Logo = () => {
-  return <Image src={eluxLogo} alt="Elux Logo" />
+interface Props {
+  className?: string
+}
+
+export const Logo: React.FC<Props> = ({ className }) => {
+  return <Image src={eluxLogo} alt="Elux Logo" className={`h-12 w-12 ${className}`}/>
 }
