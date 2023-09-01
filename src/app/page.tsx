93799c80be1678adc9b4a792e11681b79cc7d247
PabloChavez03 <pablooscarchavez@gmail.com>
2023-08-31 21:50:40 -0300

    add new items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import { SideMenu } from './components/widgets/SideMenu'
+import { AccountSVG, BillingSVG, InvoicesSVG, SettingsSVG, TeamsSVG } from './components/assets/Icons'
+import { SideMenu, SidemenuItem } from './components/widgets/SideMenu'
 
 export default function Home () {
   return (
     <main className="flex flex-col">
-      <SideMenu />
+      <SideMenu>
+        <SidemenuItem icon={<SettingsSVG />} text='General'/>
+        <SidemenuItem icon={<TeamsSVG />} text='Teams' />
+        <SidemenuItem icon={<BillingSVG />} text='Billing' active/>
+        <SidemenuItem icon={<InvoicesSVG />} text='Invoices' alert />
+        <SidemenuItem icon={<AccountSVG />} text='Account' />
+      </SideMenu>
     </main>
   )
 }
