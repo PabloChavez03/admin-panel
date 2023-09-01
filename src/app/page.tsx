6f95eec981f087601af25a791fb10578187a3193
PabloChavez03 <pablooscarchavez@gmail.com>
2023-09-01 12:51:11 -0300

    refact main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,6 @@
-import { AccountSVG, BillingSVG, InvoicesSVG, SettingsSVG, TeamsSVG } from './components/assets/Icons'
-import { SideMenu, SidemenuItem } from './components/widgets/SideMenu'
-
 export default function Home () {
   return (
     <main className="flex flex-col">
-      <SideMenu>
-        <SidemenuItem icon={<SettingsSVG />} text='General'/>
-        <SidemenuItem icon={<TeamsSVG />} text='Teams' />
-        <SidemenuItem icon={<BillingSVG />} text='Billing' active/>
-        <SidemenuItem icon={<InvoicesSVG />} text='Invoices' alert />
-        <SidemenuItem icon={<AccountSVG />} text='Account' />
-      </SideMenu>
     </main>
   )
 }
