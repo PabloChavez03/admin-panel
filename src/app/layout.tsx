1b42354b31663e4cefff9d956f98743c8cade92d
PabloChavez03 <pablooscarchavez@gmail.com>
2023-09-01 12:51:01 -0300

    add sidemenu in body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { SideMenu } from './components/widgets/SideMenu'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,12 @@ export default function RootLayout ({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='flex'>
+          <SideMenu />
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
